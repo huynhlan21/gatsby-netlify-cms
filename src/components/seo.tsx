dec42a82480102500d3854296d9e30ed10f2ed6e
Huynh Lan <huynhlan.fe21@gmail.com>
2025-01-18 01:08:15 +0700

    add seo component

---
+++ src/components/seo.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ReactNode } from "react";
+import { useSiteMetadata } from "../hooks/useSiteMetadata";
+
+export const SEO = ({
+  title,
+  description,
+  pathname,
+  children,
+}: {
+  title?: string;
+  description?: string;
+  pathname?: string;
+  children?: ReactNode;
+}) => {
+  const {
+    title: defaultTitle,
+    description: defaultDescription,
+    siteUrl,
+  } = useSiteMetadata();
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    url: `${siteUrl}${pathname || ""}`,
+  };
+
+  return (
+    <>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
+      {children}
+    </>
+  );
+};
