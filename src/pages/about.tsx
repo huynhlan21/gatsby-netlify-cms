03cf984bbe511ef05e3b4903879ae106d9a4771e
Huynh Lan <huynhlan.fe21@gmail.com>
2025-01-18 13:59:40 +0700

    using StaticImage plugin

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link, type HeadFC, type PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import { SEO } from "../components/seo";
 import Layout from "../components/layout";
 
@@ -7,6 +8,10 @@ const AboutPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="About page">
       <p>Hi there! I build this site with Gatsby follow its tutorial.</p>
+      <StaticImage
+        alt="The flower that best decribes my name"
+        src="../images/orchird.jpg"
+      />
     </Layout>
   );
 };
