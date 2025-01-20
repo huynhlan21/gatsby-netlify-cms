5936e6198918c424fa110eb97fc5e5d9bb790282
Huynh Lan <huynhlan.fe21@gmail.com>
2025-01-20 11:37:52 +0700

    add dynamic images from data layer

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { graphql, Link } from "gatsby";
+import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 import { SEO } from "../../components/seo";
 
@@ -8,6 +9,14 @@ interface BlogPostData {
     frontmatter: {
       title: string;
       date: string;
+      hero_image: {
+        childImageSharp: {
+          gatsbyImageData: IGatsbyImageData;
+        };
+      };
+      hero_image_alt: string;
+      hero_image_credit_text: string;
+      hero_image_credit_link: string;
     };
   };
 }
@@ -19,10 +28,25 @@ const BlogPost = ({
   data: BlogPostData;
   children: React.ReactNode;
 }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p className="text-sm text-gray-600">{data.mdx.frontmatter.date}</p>
       {children}
+      {image && (
+        <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      )}
+      <p>
+        Photo Credit:{" "}
+        <a
+          href={data.mdx.frontmatter.hero_image_credit_link}
+          target="_blank"
+          className="text-blue-500 underline"
+        >
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <Link to="/blog" className="text-sm text-blue-500 underline">
         Back
       </Link>
@@ -32,10 +56,18 @@ const BlogPost = ({
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: { eq: $id }, frontmatter: {}) {
+    mdx(id: { eq: $id }) {
       frontmatter {
-        date
         title
+        date
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
